7cbdfbef99d50264895ccda51bc055ec1246ad97
ven1k_mu <venik.butenko@gmail.com>
2025-04-07 11:05:13 +0300

    My test project for learning React. The project is created and ready for tasks.

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Список працівників в компаніє</h1>
+            <h2>Загальна кількість працівників: {}</h2>
+            <h2>Премію отримають: {}</h2>
+        </div>
+    )
+}
+
+export default AppInfo;  
