a4b9f2cf3b56ca2e6f8c84c6c12c9853791d8d00
ven1k_mu <venik.butenko@gmail.com>
2025-04-07 15:52:14 +0300

    Added props to the project. Added dynamic addition (creation) of the user field from the passed data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 function App() {
+    const data = [
+        {name: "Hakate Kakashi", salary: 5000, increase: false},
+        {name: "Kamado Tanjiro", salary: 2400, increase: true},
+        {name: "Ranonoa Zoro", salary: 4000, increase: false},
+        {name: "Ichigo Kurasaki", salary: 1100, increase: false},
+    ]
+
     return (
         <div className="app">
             <AppInfo />
@@ -13,10 +20,10 @@ function App() {
                 <SearchPanel />
                 <AppFilter />
             </div>
-            <EmployeesList />
+            <EmployeesList data={data}/>
             <EmployeesAddForm />
         </div>
     );
 }
 
-export default App;
+export default App; 
